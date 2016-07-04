9fc3bdb658075d4c199e723c7366843b4167f033
Markus Wolf <mail@markus-wolf.de>
2016-07-04 23:32:48 +0200

    feat: adds test setup (ui and node)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const node_modules = fs.readdirSync('node_modules').filter(x => x !== '.bin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const globby = require('globby');
+
+fs.writeFileSync('test/all.ts',
+  globby.sync(['test/**/*-test.ts', 'test/**/*-test.tsx'])
+    .map(file => file.replace('test/', '').replace(/\.tsx?$/, ''))
+    .map(file => `import './${file}';`)
+    .join('\n'));
 
 module.exports = [{
   target: 'web',
@@ -52,5 +59,30 @@ module.exports = [{
   devtool: 'source-map',
   resolve: {
     extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
+  }
+},{
+  target: 'node',
+  entry: './test/all',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'test.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript?useBabel=true'
+      },
+      {
+        test: /\.less$/,
+        loader: 'style!css!less'
+      }
+    ]
+  },
+  externals: node_modules,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.tsx', '.ts', '.webpack.js', '.web.js', '.js']
   }
 }];
