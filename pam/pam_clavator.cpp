cd43c99184f45801201e7c70810513e0c89a6165
Meno Abels <meno.abels@adviser.com>
2016-07-17 21:04:52 +0200

    update

---
+++ pam/pam_clavator.cpp
@@ -0,0 +1,226 @@
+/*-
+ */
+
+#include <sys/param.h>
+
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <libgen.h>
+#include <sys/stat.h>
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+namespace fs = boost::filesystem;
+
+#define PAM_DEBUG
+#ifdef PAM_DEBUG
+#ifndef __APPLE_CC__
+#define _PAM_LOGFILE "/run/pam-debug.log"
+#include <security/_pam_macros.h>
+#else
+#  define D(x) do {                                                     \
+    std::cout << "debug: " << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << "): " << x << std::endl; \
+  } while (0)
+#endif
+#endif
+
+#include <security/pam_modutil.h>
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+
+#include "pem_ssh.hpp"
+#include "system_cmd.hpp"
+
+
+
+class Config {
+private:
+  std::vector<BaseMatcher*> matchers;
+public:
+  Matcher<std::string> ssh_authorized_keys_fname;
+  Matcher<std::string> password_prompt;
+  Matcher<std::string> gpg_connect_agent;
+  Matcher<std::string> gpg;
+  Matcher<std::string> gpg_agent_conf;
+  Matcher<std::string> gpg_conf;
+  Matcher<bool> debug;
+  Config() :
+    ssh_authorized_keys_fname("ssh_authorized_keys_fname=", ".ssh/authorized_keys", matchers),
+    password_prompt("password_prompt=", "pincode: ", matchers),
+    gpg_connect_agent("gpg_connect_agent=", "/usr/local/bin/gpg-connect-agent", matchers),
+    gpg("gpg=", "/usr/local/bin/gpg2", matchers),
+    gpg_agent_conf("gpg_agent_conf=", ".gnupg/gpg-agent.conf", matchers),
+    gpg_conf("gpg_conf=", ".gnupg/gpg.conf", matchers),
+    debug("debug", false, matchers) {
+ }
+
+  void parse_cfg(int flags, int argc, const char **argv) {
+    for (int i = 0; i < argc; ++i) {
+      for (auto &matcher : matchers) {
+        if (matcher->match(argv[i])) {
+          break;
+        }
+      }
+    }
+    if (debug.value) {
+      for (auto &matcher : matchers) {
+          matcher->dump();
+      }
+    }
+  }
+};
+
+std::string substPattern(const char *pat, const char *val, const std::string &str) {
+  // boost::regex_replace(out, s.begin(), s.end(),
+  //     e1, format_string, boost::match_default | boost::format_all);
+  std::stringstream s2;
+  s2 << val;
+  s2 << "/";
+  s2 << str;
+  return s2.str();
+}
+
+std::string DirName(std::string source) {
+    source.erase(std::find(source.rbegin(), source.rend(), '/').base(), source.end());
+    return source;
+}
+
+
+extern "C" {
+
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
+                                   const char *argv[]) {
+  struct pam_conv *conv;
+  struct pam_message msg;
+  const struct pam_message *msgp;
+  struct pam_response *resp;
+  struct passwd *pwd;
+  const char *user;
+  //char *crypt_password, *password;
+  int pam_err, retry;
+  Config cfg;
+
+  D(("pam_sm_authenticate"));
+  cfg.parse_cfg(flags, argc, argv);
+
+  /* identify user */
+  if ((pam_err = pam_get_user(pamh, &user, NULL)) != PAM_SUCCESS) {
+    return (pam_err);
+  }
+  if ((pwd = getpwnam(user)) == NULL) {
+    return (PAM_USER_UNKNOWN);
+  }
+  /* get password */
+  pam_err = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
+  if (pam_err != PAM_SUCCESS) {
+    return (PAM_SYSTEM_ERR);
+  }
+  msg.msg_style = PAM_PROMPT_ECHO_OFF;
+  msg.msg = (char *)cfg.password_prompt.value.c_str();
+  msgp = &msg;
+
+  std::string password;
+  for (retry = 0; retry < 3; ++retry) {
+    // pam_err = pam_get_authtok(pamh, PAM_AUTHTOK, (const char **)&password,
+    // NULL);
+    resp = NULL;
+    pam_err = (*conv->conv)(1, &msgp, &resp, conv->appdata_ptr);
+    if (resp != NULL) {
+      if (pam_err == PAM_SUCCESS) {
+        password = resp->resp;
+      }
+      free(resp->resp);
+      free(resp);
+    }
+    if (pam_err == PAM_SUCCESS) {
+      break;
+    }
+  }
+  if (pam_err == PAM_CONV_ERR) {
+    return (pam_err);
+  }
+  if (pam_err != PAM_SUCCESS) {
+    return (PAM_AUTH_ERR);
+  }
+
+  std::string pin("PIN:");
+  pin += password;
+  D((pin.c_str()));
+
+  // D((substPattern("HOMEDIR", pwd->pw_dir, cfg.ssh_authorized_keys_fname.value).c_str()));
+  // D((substPattern("HOMEDIR", pwd->pw_dir, cfg.gpg_agent_conf.value).c_str()));
+  // D((substPattern("HOMEDIR", pwd->pw_dir, cfg.gpg_conf.value).c_str()));
+
+  auto gpgConfFile = substPattern("HOMEDIR", pwd->pw_dir, cfg.gpg_conf.value);
+  fs::path dotGnupgDir(fs::path(gpgConfFile.c_str()).remove_filename());
+  if (!fs::is_directory(dotGnupgDir)) {
+    SystemCmd mkdir_p(pwd, "/bin/mkdir");
+    mkdir_p.arg("-p");
+    mkdir_p.arg("--mode=0700");
+    mkdir_p.arg(dotGnupgDir.c_str());
+    mkdir_p.run(pamh);
+    if (mkdir_p.getStatus()) {
+      D((mkdir_p.dump().c_str()));
+      return PAM_AUTH_ERR;
+    }
+  }
+
+  SystemCmd kill_gpg_connect_agent(pwd, cfg.gpg_connect_agent.value);
+  kill_gpg_connect_agent.arg("--no-autostart");
+  kill_gpg_connect_agent.arg("KILLAGENT");
+  kill_gpg_connect_agent.arg("/bye");
+  kill_gpg_connect_agent.run(pamh);
+  if (kill_gpg_connect_agent.getStatus()) {
+    D((kill_gpg_connect_agent.dump().c_str()));
+    return PAM_AUTH_ERR;
+  }
+  /* compare passwords */
+  // if ((!pwd->pw_passwd[0] && (flags & PAM_DISALLOW_NULL_AUTHTOK)) ||
+  //     (crypt_password = crypt(password, pwd->pw_passwd)) == NULL ||
+  //     strcmp(crypt_password, pwd->pw_passwd) != 0)
+  //pam_err = PAM_AUTH_ERR;
+  // else
+  	pam_err = PAM_SUCCESS;
+  return (pam_err);
+}
+
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc,
+                              const char *argv[]) {
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc,
+                                const char *argv[]) {
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc,
+                                   const char *argv[]) {
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc,
+                                    const char *argv[]) {
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc,
+                                const char *argv[]) {
+  return (PAM_SERVICE_ERR);
+}
+
+#ifdef PAM_MODULE_ENTRY
+PAM_MODULE_ENTRY("pam_clavator");
+#endif
+}
