409450573f3a8b5d7f81db9805d3f03fc824ca95
Meno Abels <meno.abels@adviser.com>
2016-07-17 21:44:36 +0200

    update

---
+++ pam/matcher.hpp
@@ -0,0 +1,47 @@
+class BaseMatcher {
+public:
+  virtual ~BaseMatcher() {}
+  virtual bool match(const char *argv) = 0;
+  virtual void dump() const = 0;
+};
+
+template<class T>
+class Matcher  : public BaseMatcher {
+private:
+  const char *matchString;
+  T preset;
+public:
+  T value;
+  Matcher(const char *match, const T &value, std::vector<BaseMatcher *> &matcher)
+    : matchString(match), preset(value), value(value) {
+    matcher.push_back(this);
+  }
+  virtual ~Matcher() {}
+  virtual bool match(const char *argv);
+  virtual void dump() const {
+      std::stringstream s2;
+      if (matchString[strlen(matchString)-1] == '=') {
+        s2 << matchString << value << " preset=" << preset;
+      } else {
+        s2 << matchString << "=" << value << " preset=" << preset;
+      }
+      D((s2.str().c_str()));
+  }
+
+};
+
+template<> bool Matcher<std::string>::match(const char *argv) {
+  if (!strncmp(argv, matchString, strlen(matchString)-1)) {
+      value = argv + strlen(matchString);
+      return true;
+  }
+  return false;
+}
+
+template<> bool Matcher<bool>::match(const char *argv) {
+  if (!strncmp(argv, matchString, strlen(matchString)-1)) {
+      value = true;
+      return true;
+  }
+  return false;
+}
---
+++ pam/system_cmd.hpp
@@ -0,0 +1,78 @@
+
+class SystemCmd {
+private:
+  const struct passwd *pwd;
+  std::vector<std::string> args;
+  std::vector<std::string> envs;
+  const std::string exec;
+  int status;
+public:
+  SystemCmd(const struct passwd *pwd, const std::string &cmd) : pwd(pwd), exec(cmd), status(-1) {
+      env("HOMEDIR", pwd->pw_dir);
+      arg(exec);
+  }
+  SystemCmd(const struct passwd *pwd, const char *cmd) : pwd(pwd), exec(cmd), status(-1) {
+      env("HOMEDIR", pwd->pw_dir);
+      arg(exec);
+  }
+
+  int getStatus() {
+    return status;
+  }
+
+  void env(const char *key, const char *value)  {
+    std::string kv(key);
+    kv += "=";
+    kv += value;
+    envs.push_back(kv);
+  }
+
+  void arg(const char *part) { args.push_back(part); }
+  void arg(const std::string &part) { args.push_back(part); }
+
+  std::string dump() const {
+    std::stringstream ret;
+    ret << getuid() << ":" << pwd->pw_uid << ":" << pwd->pw_gid << "=>";
+    const char *space = "";
+    for (auto &v : envs) {
+        ret << space << v;
+        space = " ";
+    }
+    ret << space << exec;
+    for (auto &v : args) {
+        ret << space << v;
+        space = " ";
+    }
+    if (status >= 0) {
+      ret << "=>" << status;
+    }
+    return ret.str();
+  }
+  int run(pam_handle_t *pamh) {
+    pid_t pid = fork();
+    if (pid == 0) {
+      PAM_MODUTIL_DEF_PRIVS(privs);
+      int ret = pam_modutil_drop_priv(pamh, &privs, pwd);
+      setuid(pwd->pw_uid);
+      seteuid(pwd->pw_uid);
+      char *argv[args.size()+1];
+      argv[args.size()] = 0;
+      for (size_t i = 0; i < args.size(); ++i) {
+          argv[i] = (char *)args[i].c_str();
+      }
+      char *envp[envs.size()+1];
+      envp[envs.size()] = 0;
+      for (size_t i = 0; i < envs.size(); ++i) {
+          envp[i] = (char *)envs[i].c_str();
+      }
+      D((dump().c_str()));
+      ret = execve(exec.c_str(), argv, envp);
+      //std::cout << ">>>>" << pwd->pw_uid << ":" << getuid() <<
+      //  ":" << ret << ":" << privs.is_dropped << std::endl;
+      return ret;
+    } else {
+      waitpid(pid, &status, 0);
+      return status;
+    }
+  }
+};
