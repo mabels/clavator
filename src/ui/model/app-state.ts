13f2bcb7549872ffd608865e6a6ac5bef9bf14e1
Meno Abels <meno.abels@adviser.com>
2017-11-21 11:50:04 +0100

    * missing file

---
+++ src/ui/model/app-state.ts
@@ -0,0 +1,26 @@
+
+import * as WsChannel from '../model/ws-channel';
+import { CardStatusListState } from './card-status-list-state';
+import { KeyChainListState } from './key-chain-list-state';
+import { ProgressorState } from '../components/controls/progressor';
+import { AssistentState } from '../components/assistent';
+
+export default class AppState {
+      public readonly channel: WsChannel.Dispatch;
+      public readonly cardStatusListState: CardStatusListState;
+      public readonly keyChainListState: KeyChainListState;
+      public readonly progressorState: ProgressorState;
+      public readonly assistentState: AssistentState;
+
+      public static create(): AppState {
+            return new AppState();
+      }
+
+      private constructor() {
+            this.channel = WsChannel.Dispatch.create();
+            this.cardStatusListState = new CardStatusListState(this.channel);
+            this.keyChainListState = new KeyChainListState(this.channel);
+            this.progressorState = new ProgressorState(this.channel);
+            this.assistentState = new AssistentState();
+      }
+}
